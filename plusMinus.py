b0b06075011454d5e70dacaa03360b65cb79cb74
lucasmassarico <lucasmassarico1@gmail.com>
2022-11-14 20:41:17 -0300

    Commit

---
+++ plusMinus.py
@@ -0,0 +1,28 @@
+#
+# Complete the 'plusMinus' function below.
+#
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def plusMinus(arr):
+    cont_plus = 0
+    cont_nega = 0
+    cont_zero = 0
+    for i in range(arr.__len__()):
+        if arr[i] > 0:
+            cont_plus += 1
+        elif arr[i] < 0:
+            cont_nega += 1
+        else:
+            cont_zero += 1
+    print(round(cont_plus / len(arr), 6))
+    print(round(cont_nega / len(arr), 6))
+    print(round(cont_zero / len(arr), 6))
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    plusMinus(arr)
