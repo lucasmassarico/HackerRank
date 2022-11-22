e89739556fde3cf89002d537a32cccb9400f1159
Lucas Massarico <lucasmassarico1@gmail.com>
2022-11-22 09:53:10 -0300

    No Pairs Allowed

---
+++ no_pairs_allowed.py
@@ -0,0 +1,21 @@
+def minimalOperations(lista):
+    count = []
+    for word in lista:
+        word = list(word)
+        i = 0
+        count_letter = 0
+        while i < len(word) - 1:
+            if word[i] == word[i + 1]:
+                count_letter = 1
+            i += 1
+        count.append(count_letter)
+    return count
+
+
+if __name__ == '__main__':
+    n = int(input())
+    lista = []
+    for _ in range(n):
+        lista.append(input(""))
+    result = minimalOperations(lista)
+    print(result)
