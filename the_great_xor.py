1d65cc956c4ef292d9fb0846978054ab557ecfaa
Lucas Massarico <lucasmassarico1@gmail.com>
2023-01-25 16:04:55 -0300

    Commit

---
+++ the_great_xor.py
@@ -0,0 +1,16 @@
+def the_great_xor(xor):
+    count = 0
+    for interactions in range(xor):
+        # Verificando se o resultado da operação XOR é maior que x
+        if interactions ^ xor > xor:
+            # Se sim, incrementando o contador
+            count += 1
+    return count
+
+
+if __name__ == '__main__':
+    rounds = int(input().strip())
+
+    for rounds_itr in range(rounds):
+        number = int(input().strip())
+        print(the_great_xor(number))
